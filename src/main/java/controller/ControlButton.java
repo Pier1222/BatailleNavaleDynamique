84ec8ffca6962680fca330e9a7d89ba65daf704e
Rosendark <remirosenblatt@gmail.com>
2019-03-29 11:40:26 +0100

    début de test

--- src/main/java/controller/ControlButton.java
+++ src/main/java/controller/ControlButton.java
@@ -1,13 +1,39 @@
 package controller;
 
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
 import model.Bataille_navale_model;
-import model.Game;
+import model.Son;
 import view.View;
 
-public class ControlButton {
+public class ControlButton implements ActionListener {
+	
+	Bataille_navale_model model;
+	View view;
+	Son son;
+	
 
-	public ControlButton(Bataille_navale_model model, Game game, View view) {
-		// TODO Auto-generated constructor stub
+
+	public ControlButton(Bataille_navale_model model, Son son, View view) {
+		this.model = model;
+        this.view = view;
+        this.son = son;
+        
+        son.jouer();
+        
+		view.setButtonControler(this);
+	
+	}
+	
+	public void actionPerformed(ActionEvent e){
+		
+		if(e.getSource() == view.launchButton){
+			
+			 
+		
+		}
+		
 	}
 
 }
--- src/main/java/launcher/Launcher.java
+++ src/main/java/launcher/Launcher.java
@@ -1,9 +1,33 @@
 package launcher;
 
+import model.Son;
+import controller.ControlGroup;
+import model.Bataille_navale_model;
+
 public class Launcher {
 
 	public static void main(String[] args) {
 		
+		
+		   javax.swing.SwingUtilities.invokeLater( new Runnable() {
+	            public void run() {
+	
+					Bataille_navale_model model = new Bataille_navale_model();
+					try {
+						ControlGroup controlgroup = new ControlGroup(model);
+					} catch (Exception e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					}
+					
+	
+		    
+	            }
+	        });
+		   
+		   
+		   
+	 
 	}
 	
 }
