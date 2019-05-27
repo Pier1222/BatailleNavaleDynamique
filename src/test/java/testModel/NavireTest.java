8d813fb3a3aa8866a9049f42bca24210a529fcf6
Rosendark <remirosenblatt@gmail.com>
2019-05-27 08:47:23 +0200

    rien

--- src/test/java/testModel/NavireTest.java
+++ src/test/java/testModel/NavireTest.java
@@ -4,10 +4,14 @@ import static org.junit.Assert.*;
 
 import org.junit.Test;
 
+import model.Son;
+
+
 public class NavireTest {
 	
 	  @Test
 	    public void test() {
+		  	Son son = new Son("aaa");
 	        assertEquals(4, 2 + 2);
 	    }
 }
